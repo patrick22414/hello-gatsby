332e95079ac33d0a250940d66bed2f191e5fcf67
Patrick Yang <ravencrown.ysy@gmail.com>
2019-05-23 14:36:38 +0800

    2019-05-23

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
+
+export default () => (
+    <div style={{ color: `teal` }}>
+        <Link to="/">Home</Link>
+        <Header headerText="About Header Text" />
+        <Header headerText="Let's have another header" />
+        <p>Gatsby about page.</p>
+    </div>
+)

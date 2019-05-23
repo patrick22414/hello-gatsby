0567c06b22d0427a731539fadeb5043212b41aa2
Patrick Yang <ravencrown.ysy@gmail.com>
2019-05-23 18:05:36 +0800

    styles!

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,10 +3,10 @@ import Header from "../components/header"
 import { Link } from "gatsby"
 
 export default () => (
-    <div style={{ color: `teal` }}>
-        <Link to="/">Home</Link>
-        <Header headerText="About Header Text" />
-        <Header headerText="Let's have another header" />
-        <p>Gatsby about page.</p>
-    </div>
+  <div style={{ color: `teal` }}>
+    <Link to="/">Home</Link>
+    <Header headerText="About Header Text" />
+    <Header headerText="Let's have another header" />
+    <p>Gatsby about page.</p>
+  </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 
 export default () => (
-    <div style={{ color: `purple` }}>
-        <Link to="/about/">About</Link>
-        <h1>Hello Gatsby!</h1>
-        <p>What a world.</p>
-        <img src="https://source.unsplash.com/random/400x200" alt="some random image" />
-    </div>
+  <div style={{ color: `purple` }}>
+    <Link to="/about/">About</Link>
+    <br />
+    <Link to="/tutorial-css-modules/">Tutorial: CSS Modules</Link>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/360x240" alt="" />
+  </div>
 )

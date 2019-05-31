6c90bd7291cac97d466a1ca52e1f0a02567d5759
Patrick Yang <ravencrown.ysy@gmail.com>
2019-05-31 08:18:06 +0800

    I prefer this Style

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,31 @@ export default () => (
       Now with{" "}
       <a href="https://kyleamathews.github.io/typography.js/">Typography.js</a>.
     </p>
-    <img src="https://source.unsplash.com/random/360x240" alt="" />
+    <button
+      style={{
+        display: `block`,
+        margin: `1rem`,
+        padding: `0.5em 1em`,
+        background: `white`,
+        boxShadow: `0px 10px 20px rgba(0, 0, 0, 0.2)`,
+        border: `solid violet`,
+        borderRadius: `10px`,
+        color: `violet`,
+      }}
+    >
+      A button
+    </button>
+    <img
+      style={{
+        display: `block`,
+        margin: `1rem`,
+        background: `white`,
+        boxShadow: `0px 10px 20px rgba(0, 0, 0, 0.2)`,
+        border: `20px solid white`,
+        borderRadius: `10px`,
+      }}
+      src="https://source.unsplash.com/random/360x240"
+      alt=""
+    />
   </Layout>
 )

cd455a6ef1dc529617c545552a2126311072fdd8
Patrick Yang <ravencrown.ysy@gmail.com>
2019-05-31 22:24:03 +0800

    Grid!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,27 @@
 import React from "react"
 import Layout from "../components/layout"
+import imageStyles from "../components/image.module.css"
 
 export default () => (
   <Layout>
-    <h1>Hello Gatsby</h1>
-    <p>What a world.</p>
-    <p>
-      Now with{" "}
-      <a href="https://kyleamathews.github.io/typography.js/">Typography.js</a>.
-    </p>
-    <button
-      style={{
-        display: `block`,
-        margin: `1rem`,
-        padding: `0.5em 1em`,
-        background: `white`,
-        boxShadow: `0px 10px 20px rgba(0, 0, 0, 0.2)`,
-        border: `solid violet`,
-        borderRadius: `10px`,
-        color: `violet`,
-      }}
-    >
-      A button
-    </button>
-    <img
-      style={{
-        display: `block`,
-        margin: `1rem`,
-        background: `white`,
-        boxShadow: `0px 10px 20px rgba(0, 0, 0, 0.2)`,
-        border: `20px solid white`,
-        borderRadius: `10px`,
-      }}
-      src="https://source.unsplash.com/random/360x240"
-      alt=""
-    />
+    <div>
+      <h1>Hello Gatsby</h1>
+      <p>
+        Commodo cupidatat ut quis officia esse irure anim deserunt voluptate
+        occaecat non. Occaecat consectetur magna nisi quis eiusmod pariatur
+        incididunt consequat aliqua. Duis duis cillum Lorem irure dolore enim
+        amet officia cupidatat cillum. Magna ex nisi enim tempor exercitation
+        exercitation ullamco ad do incididunt enim.
+      </p>
+      <p>
+        Qui labore ex elit eiusmod occaecat cupidatat labore. Consectetur est
+        commodo ullamco ut. Voluptate magna enim exercitation labore nisi sint
+        dolor quis veniam. Commodo duis elit deserunt officia minim dolore esse.
+        Cupidatat nulla laborum aliquip velit.
+      </p>
+    </div>
+    <div className={imageStyles.myImage}>
+      <img src="https://source.unsplash.com/random/360x240" alt="" />
+    </div>
   </Layout>
 )
